b5b87157fbdfa96bd933f47293119f0b4cf25670
MartimBaptista <martimdsbaptista@gmail.com>
2022-11-08 13:46:12 +0000

    removed unnecessary .h and moved contents to .c

--- include/request_t-private.h
+++
@@ -1,7 +0,0 @@
-struct request_t { 
-    int op_n; //o número da operação 
-    int op; //a operação a executar. op=0 se for um delete, op=1 se for um put 
-    char* key; //a chave a remover ou adicionar 
-    char* data; // os dados a adicionar em caso de put, ou NULL em caso de delete 
-    //adicionar campo(s) necessário(s) para implementar fila do tipo produtor/consumidor 
-};
